16c25a4cb2064fbb92a90c4bb7234a859c502f44
GeovannyTS <yowtecum@gmail.com>
2023-09-11 15:32:15 -0600

    Peticion por medio de la region e implemeentacion del segundo selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
-import { Region } from '../../interfaces/country.interface';
+import { Region, SmallCountry } from '../../interfaces/country.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styleUrls: ['./selector-page.component.css']
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
+
+  public countriesByRegion:SmallCountry[] = [];
 
   public myForm:FormGroup = this.fb.group({
     region: ['', Validators.required],
@@ -23,8 +26,22 @@ export class SelectorPageComponent {
     private countriesService:CountriesService,
     ){}
 
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
   get regions(): Region[]{
     return this.countriesService.regions;
+  }
+
+  onRegionChanged():void{
+    this.myForm.get('region')!.valueChanges
+    .pipe(
+      switchMap(region => this.countriesService.getCountriesByRegion(region))
+    )
+    .subscribe(region =>{
+      this.countriesByRegion = region;
+    });
   }
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,17 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interface';
+import { Country, Region, SmallCountry } from '../interfaces/country.interface';
+import { Observable, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private baseUrl:string = 'https://restcountries.com/v3.1'
+
   private _regions:Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europa, Region.Oceania ];
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
 
   get regions():Region[] {
     return [ ...this._regions ];
+  }
+
+  getCountriesByRegion(region:Region):Observable<SmallCountry[]> {
+
+    //para regresar algo necesitamos que sea un observable. implementamos of para que se pueda implementar
+    if(!region) return of ([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map( countries => countries.map(country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? []
+        }))),
+        // tap( response => console.log({response})
+        // )
+      )
   }
 
 }
