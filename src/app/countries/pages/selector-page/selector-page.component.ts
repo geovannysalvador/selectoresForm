360d825131d8228ae31a167cb13fd08f76494bed
GeovannyTS <yowtecum@gmail.com>
2023-09-12 15:57:50 -0600

    limpiar selector por condicion

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -37,6 +37,8 @@ export class SelectorPageComponent implements OnInit {
   onRegionChanged():void{
     this.myForm.get('region')!.valueChanges
     .pipe(
+      //Para el reinicio del segundo formulario
+      tap( () => this.myForm.get('country')!.setValue('') ),
       switchMap(region => this.countriesService.getCountriesByRegion(region))
     )
     .subscribe(region =>{
